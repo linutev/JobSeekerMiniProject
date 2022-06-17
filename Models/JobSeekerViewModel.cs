09d2572271cc7aeef157ba0d5802706ad361d2c7
Linukas <lina.valunaite@gmail.com>
2022-06-17 13:43:01 +0100

    Initial commit

---
+++ Models/JobSeekerViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobSeekerMiniProject.Models
+{
+	public class JobSeekerViewmodel
+	{
+		[Required(ErrorMessage = "Please enter your full name.")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Please enter your email.")]
+		[EmailAddress]
+		[MaxLength(100, ErrorMessage = "Exceeded max length.")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Please enter your phone.")]
+		[RegularExpression(@"[0-9]+", ErrorMessage = "Please enter a valid phone number.")]
+		[MinLength(9, ErrorMessage = "Please enter a valid phone number.")]
+		[MaxLength(12, ErrorMessage = "Exceeded max length.")]
+		public string Phone { get; set; }
+
+		[Required(ErrorMessage = "Please enter your address.")]
+		public string Address { get; set; }
+
+		[Required(ErrorMessage = "Please enter your skills.")]
+		public string Skills { get; set; }
+	}
+}
+
